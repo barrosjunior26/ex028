0db2635b1eed64bee572d6ec93cd21068d7fc56d
barrosjunior26 <barrosjunior26@outlook.com.br>
2026-03-08 17:43:48 -0300

    Desenvolvimento de toda a aplicação e em 'bom' funcionamento.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace ex028
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Variáveis globais
+            double resultado, valor1, valor2;
+
+            Console.Clear();
+
+            Console.Write("Informe o primeiro valor: ");
+            valor1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe o segundo valor: ");
+            valor2 = Convert.ToDouble(Console.ReadLine());
+
+            resultado = Soma(valor1, valor2);
+
+            Console.WriteLine($"O resultado da soma entre {valor1} + {valor2} = {Math.Round(resultado, 2)}");
+        }
+
+        public static double Soma(double n1, double n2)
+        {
+            double resultadoSoma;
+
+            resultadoSoma = n1 + n2;
+
+            return resultadoSoma;
         }
     }
 }
