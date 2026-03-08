6045aa9fd74056396625b9fce138c74799b53f74
barrosjunior26 <barrosjunior26@outlook.com.br>
2026-03-08 17:49:17 -0300

    Inclusão do arquivo readme com a descrição do aplicativo

---
+++ README.md
@@ -0,0 +1,3 @@
+## ➕ Descrição do Aplicativo
+
+Este aplicativo em C# realiza a **soma de dois valores** informados pelo usuário. Ele lê os números pelo console, envia para uma função chamada `Soma()` e exibe o resultado final de forma simples e rápida.
--- obj/Debug/net5.0/ex028.AssemblyInfo.cs
+++ obj/Debug/net5.0/ex028.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex028")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+700579b78e5a171747fb831d7a9a0ff859c9e455")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db2635b1eed64bee572d6ec93cd21068d7fc56d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex028")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex028")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
